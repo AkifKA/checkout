63a5ed895cd4d01f082f3250fea752bcfdbbc8f4
Mehmet Akif Karaöz <karaozmehmetakif@gmail.com>
2023-07-29 20:42:00 +0300

    7. Updated ProductList.jsx and added Loading.jsx

---
+++ src/components/Loading.jsx
@@ -0,0 +1,14 @@
+const Loading = () => {
+  return (
+    <>
+      <p className="text-center text-danger w-100">Loading....</p>
+      <img
+        className="d-flex mx-auto w-50"
+        src="https://i.gifer.com/origin/34/34338d26023e5515f6cc8969aa027bca_w200.gif"
+        alt="loading"
+      />
+    </>
+  );
+};
+
+export default Loading;
--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -2,42 +2,59 @@ import React, { useState, useEffect } from "react";
 import ProductCard from "../components/ProductCard";
 import CardTotal from "../components/CardTotal";
 import axios from "axios";
+import Loading from "../components/Loading";
 
 const ProductList = () => {
   const url = process.env.REACT_APP_API_URL;
-  console.log("url:", url);
+
   const [products, setProducts] = useState([]);
 
+  const [loading, setLoading] = useState(true);
+  const [errorState, setErrorState] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
   const getProducts = async () => {
-    console.log("merhaba");
+    setLoading(false);
+
     try {
       const { data } = await axios(url);
       setProducts(data);
-      console.log(data);
+      setLoading(false);
+      setErrorState(false);
     } catch (error) {
       console.log(error);
+      setErrorMessage(error.message);
+      setErrorState(true);
     }
   };
 
   useEffect(() => {
     getProducts();
   }, []);
-
+  console.log(products.length);
   return (
     <div className="container mt-3">
       <div className={"bg-light d-sm-block d-md-flex"}>
-        <p className="text-center text-danger w-100">Loading....</p>
+        {loading ? (
+          <Loading />
+        ) : products.length > 0 ? (
+          <>
+            <article id="product-panel" className="col-md-5">
+              <ProductCard />
+            </article>
+            <article className="col-md-5 m-3">
+              <CardTotal />
+            </article>
+          </>
+        ) : (
+          !errorState && (
+            <p className="text-center text-danger w-100">No products data...</p>
+          )
+        )}
 
-        <>
-          <article id="product-panel" className="col-md-5">
-            <ProductCard />
-          </article>
-          <article className="col-md-5 m-3">
-            <CardTotal />
-          </article>
-        </>
-
-        <p className="text-center text-danger w-100">No products data...</p>
+        {errorState && (
+          <p className="text-center text-danger w-100">Error: {errorMessage}</p>
+        )}
       </div>
     </div>
   );
