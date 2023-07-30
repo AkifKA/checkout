f0eb24c69f725f099b5ed640a51c58b5c13f4510
Mehmet Akif Karaöz <karaozmehmetakif@gmail.com>
2023-07-30 07:30:43 +0300

    22. fixed

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { NavLink, useNavigate } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 import "../index.css";
 
 const Main = () => {
   return (
     <div className="container">
-      <h1 className="text-center mt-5">Welcome My Checkout Page</h1>
+      <h1 className="text-center mt-5">Welcome to My Checkout Page</h1>
       <h2 className="text-center mt-3">
         Click{" "}
         <NavLink to="/product-list" className="link-text">
